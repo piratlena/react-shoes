4c4a6930c786f0d9684199492f006aed13e49e2a
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-01-18 13:41:56 +0700

    add logic of adding sizes and closing popup

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,5 @@
 import  './card.module.scss';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Bookmark from '../../assets/img/bookmark.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import {addOneToCart} from '../../redux/redusers/cartReducer';
@@ -7,8 +7,10 @@ import {addOneToCart} from '../../redux/redusers/cartReducer';
 
 const Card = ({id, image, description, price, name, size}) => {
 const [dropDown, setDropDown] = useState(false)
+const [activeSize, setActiveSize] = React.useState(0);
 const dispatch = useDispatch()
 const cartItem = useSelector(state => state.cart.itemInCart.find(item => item.id === id))
+const sizeRef = useRef()
 
 const addedQuantity = cartItem ? cartItem.addedQuantity : 0;
 
@@ -18,18 +20,34 @@ const addedQuantity = cartItem ? cartItem.addedQuantity : 0;
             name, 
             price,
             image,
-            size
+            sizes: size[activeSize]
         };
         dispatch(addOneToCart(item))
+        setDropDown(false)
+        setActiveSize()
     }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+         if (!event.path.includes(sizeRef.current)) {
+              setDropDown(false)
+              setActiveSize()
+            }
+        }
+        document.body.addEventListener('click', handleClickOutside)
+    
+        return () => {
+          document.body.removeEventListener('click', handleClickOutside)
+        }
+      }, [])
 
     const showDropDown = () => {
         setDropDown(false)
     }
     
  return (
-    <div className="card">
-        <div className="card__bookmark">
+    <div className="card" ref={sizeRef}>
+        <div className="card__wishlist">
             <img src={Bookmark} alt="bookmark"/>
         </div>
         <img width={133} height={112} src={image}alt="Sneakers" />
@@ -40,11 +58,14 @@ const addedQuantity = cartItem ? cartItem.addedQuantity : 0;
                 <b>{price}</b>
                 </div>
                 <div className="card__plus" onClick={() => setDropDown(!dropDown)}></div>
-                {dropDown && (<div className='card__popup'>
+                {dropDown && (<div  className='card__popup'>
+                    <p className='card__popup-title'>Выберите размер</p>
                 <ul className='card__size'>
                     {size.map((item, i) => (
                         <li
                         key={i}
+                        onClick={() => setActiveSize(i)}
+                        className={activeSize===i ? 'active' : ''}
                         >{item}</li>
                     ))}
                     
